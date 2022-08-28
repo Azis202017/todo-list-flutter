42f8067ebecf0c1dbf44f943bce4bf279e3d64fb
Azis202017 <azisa6980@gmail.com>
2022-08-28 18:16:52 +0700

    Add dependency and onboard
    Add shared for the themes and color

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,29 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/onboard/bindings/onboard_binding.dart';
+import '../modules/onboard/views/onboard_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.ONBOARD;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.ONBOARD,
+      page: () => const OnboardView(),
+      binding: OnboardBinding(),
     ),
   ];
 }
---
+++ lib/app/shared/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/app/shared/color.dart';
+
+ThemeData theme = ThemeData(
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: primaryColor,
+  ),
+);
